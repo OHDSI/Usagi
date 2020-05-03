795c33a7f5309c3ec302c93fb651b4037747b789
Maxim Moinat <maxim@thehyve.nl>
2020-05-03 10:48:20 +0200

    ask to export unapproved mappings

---
+++ src/org/ohdsi/usagi/ui/actions/Dialogs.java
@@ -0,0 +1,30 @@
+package org.ohdsi.usagi.ui.actions;
+
+import org.ohdsi.usagi.ui.Global;
+
+import javax.swing.*;
+
+public class Dialogs {
+    public static boolean askExportUnapprovedMappings() {
+        int PromptResult = JOptionPane.showOptionDialog(
+                Global.frame,
+                "Do you want to export both approved and unapproved mappings?",
+                "Export for review",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new String [] {"Only approved","Approved and Unapproved"},
+                "Only approved"
+        );
+        return PromptResult == JOptionPane.YES_OPTION;
+    }
+
+    public static void warningNothingToExport() {
+        JOptionPane.showMessageDialog(
+                Global.frame,
+                "There are no approved mappings, so nothing to export",
+                "Nothing to export",
+                JOptionPane.WARNING_MESSAGE
+        );
+    }
+}
