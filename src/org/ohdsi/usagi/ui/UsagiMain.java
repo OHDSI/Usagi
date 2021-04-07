1ec713db94cc9e6bd9caf87e7be4162a56c24fa3
Maxim Moinat <maxim@thehyve.nl>
2021-04-07 12:50:35 +0200

    bump version

--- src/org/ohdsi/usagi/ui/UsagiMain.java
+++ src/org/ohdsi/usagi/ui/UsagiMain.java
@@ -41,7 +41,7 @@ import org.ohdsi.utilities.files.ReadTextFile;
  */
 public class UsagiMain implements ActionListener {
 
-	public static String version = "1.4.3-SNAPSHOT";
+	public static String version = "1.4.3";
 
 	public static void main(String[] args) {
 		new UsagiMain(args);
